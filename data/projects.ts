c68b49c3a836e6a9fdb603c14ad99f27ce7b4ade
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-04-21 10:11:57 +0700

    Create projects array with project details
    Added an array of projects with details including title, location, year, image, and description.

---
+++ data/projects.ts
@@ -0,0 +1,52 @@
+import { Project } from "@/types/project";
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    title: "Gedung Perkantoran Jakarta",
+    location: "Jakarta Selatan",
+    year: "2023",
+    image: "/images/projects/project-1.jpg",
+    description: "Pembangunan gedung 20 lantai dengan struktur baja WF",
+  },
+  {
+    id: 2,
+    title: "Jembatan Layang Surabaya",
+    location: "Surabaya, Jawa Timur",
+    year: "2022",
+    image: "/images/projects/project-2.jpg",
+    description: "Konstruksi jembatan dengan besi beton berkualitas tinggi",
+  },
+  {
+    id: 3,
+    title: "Pabrik Manufaktur Bekasi",
+    location: "Bekasi, Jawa Barat",
+    year: "2023",
+    image: "/images/projects/project-3.jpg",
+    description: "Pembangunan pabrik seluas 10.000 m2 dengan struktur baja",
+  },
+  {
+    id: 4,
+    title: "Apartemen Bandung",
+    location: "Bandung, Jawa Barat",
+    year: "2023",
+    image: "/images/projects/project-1.jpg",
+    description: "Proyek apartemen 2 tower dengan 500 unit",
+  },
+  {
+    id: 5,
+    title: "Ruko 3 Lantai",
+    location: "Tangerang, Banten",
+    year: "2022",
+    image: "/images/projects/project-2.jpg",
+    description: "Pembangunan 50 unit ruko modern",
+  },
+  {
+    id: 6,
+    title: "Gudang Logistik",
+    location: "Cikarang, Jawa Barat",
+    year: "2023",
+    image: "/images/projects/project-3.jpg",
+    description: "Gudang penyimpanan seluas 5.000 m2",
+  },
+];
