220122ec4247813df4b1c798607c92ea776b1523
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-04-21 12:36:37 +0700

    Add TrustedPartners component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,13 @@ import ProductHighlights from "@/components/home/ProductHighlights";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 import ProjectPreview from "@/components/home/ProjectPreview";
 import CTASection from "@/components/home/CTASection";
+import TrustedPartners from "@/components/home/TrustedPartners";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <TrustedPartners />
       <AboutPreview />
       <ProductHighlights />
       <WhyChooseUs />
