0d4884700b617408a0c6342fa654433fa56f890a
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-04-21 10:10:53 +0700

    Add companyInfo object with details for PT Hais Prima

---
+++ data/company.ts
@@ -0,0 +1,23 @@
+export const companyInfo = {
+  name: "PT Hais Prima Indonesia",
+  shortName: "Hais Prima",
+  description: "Supplier besi dan baja terpercaya di Indonesia",
+  founded: 2015,
+  vision: "Menjadi mitra terpercaya dalam pembangunan Indonesia",
+  mission: [
+    "Menyediakan material berkualitas dengan harga kompetitif",
+    "Memberikan pelayanan terbaik kepada pelanggan",
+    "Membangun kemitraan jangka panjang yang saling menguntungkan",
+  ],
+  contact: {
+    phone: "+62 21 1234 5678",
+    email: "info@haisprima.com",
+    address: "Jl. Raya Industri No. 123, Kawasan Industri Pulogadung, Jakarta Timur 13930",
+    whatsapp: "6281234567890",
+  },
+  socialMedia: {
+    facebook: "https://facebook.com/haisprima",
+    instagram: "https://instagram.com/haisprima",
+    linkedin: "https://linkedin.com/company/haisprima",
+  },
+};
